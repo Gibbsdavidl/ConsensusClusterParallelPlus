6f1d3846dc3e341dd42529436bede0f2d9828e00
David L Gibbs <gibbsdavidl@gmail.com>
2017-01-21 13:27:53 -0800

    added new par implementation

--- test.R
+++ test.R
@@ -20,45 +20,78 @@ for (r in 1:repCount) {
 }
 
 src <- '
-  Rcpp::List cas(a);
-  Rcpp::List mat(b);
-  Rcpp::
-
-  Rcpp::NumericVector clusterAssignments(a);
-  Rcpp::NumericMatrix bm(b);
-  arma::mat m = Rcpp::as<arma::mat>(bm);
-  Rcpp::NumericVector sampleKey(c);
-  Rcpp::NumericVector kmax(d);
+  Rcpp::List parRes(a);            // results from the parallel runs
+  Rcpp::NumericMatrix mCount(b);   // mCount matrix
+  Rcpp::List ml(c);                // list of ml matrices
+  Rcpp::NumericVector kmax(d);     // the kmax
   int kint = kmax[0];
+  Rcpp::NumericVector rc(e);
+  int repCount = rc[0];
 
-  Rcpp::List cls(kint); // list of binary vectors of sample memebership
-  int n = bm.ncol(); // number of samples
+  for (int r=0; r < repCount; ++r) {    // for each rep
+      for (int k=1; r < kint; ++k) {      // for each k
 
-  for (int ki=1; ki < kint; ++ki) {      // for each cluster assignment possible
-    Rcpp::NumericVector x(n);            //    vector for each sample in n
-    for (int si=0; si < clusterAssignments.size(); ++si) {    // for each sample
-      if (clusterAssignments[si] == ki) {                     // if the sample is in the cluster
-        x[(sampleKey[si])-1] = 1;
-      } else {
-        x[(sampleKey[si])-1] = 0;
-      }
-    }
-    cls[ki] = x;
-  }
+        Rcpp::List extract1(parRes[r]);     // extract from parRes
+        Rcpp::List extract2(extract1[0]);
+        Rcpp::NumericVector clusterAssignments(extract2[k]);
+        Rcpp::NumericVector sampleKey(extract2[3]);
+        Rcpp::NumericVector mCountAssignments(clusterAssignments.size(), 1.0);
 
-  for (int ki=2; ki <= kint; ++ki) { // for each cluster
-    arma::mat xx(n,n,arma::fill::zeros);  // empty matix
-    IntegerVector a(cls[(ki-1)]);
-    for (int i=0; i < n; ++i) {
-      for (int j=0; j < n; ++j) {
-        xx(i,j) = a[i] * a[j];
-      }
-    }
-    m = m+xx;
-  }
-  return(Rcpp::wrap(m));
+        Rcpp::NumericMatrix bm = ml[k];                       // extract from ml
+        int n = bm.ncol(); // number of samples
+        arma::mat mlmat = Rcpp::as<arma::mat>(bm);
+        arma::mat mcmat = Rcpp::as<arma::mat>(mCount);
+
+        Rcpp::List cls_mCount(kint); // list of binary vectors of sample memebership
+        Rcpp::List cls_ml(kint);     // list of binary vectors of sample memebership
+
+        // for mCount number of possible clusters is always 1
+        for (int si=0; si < clusterAssignments.size(); ++si) {    // for each sample
+            Rcpp::NumericVector y(n);                                //    vector for each sample in n
+            if (mCountAssignments[si] == 1) {                       // if the sample is in the cluster
+                y[(sampleKey[si])-1] = 1;
+            } else {
+                y[(sampleKey[si])-1] = 0;
+            }
+            cls_mCount[0] = y;
+        }
+
+        // for ml
+        int thisKMax = max(clusterAssignments);
+        for (int ki=1; ki < thisKMax; ++ki) {      // for each cluster assignment possible
+            Rcpp::NumericVector x(n);               //    vector for each sample in n
+            for (int si=0; si < clusterAssignments.size(); ++si) {    // for each sample
+                if (clusterAssignments[si] == ki) {                     // if the sample is in the cluster
+                    x[(sampleKey[si])-1] = 1;
+                } else {
+                    x[(sampleKey[si])-1] = 0;
+                }
+            }
+            cls_ml[ki] = x;
+        }
+
+        arma::mat xx(n,n,arma::fill::zeros);  // empty matix
+        IntegerVector a(cls_mCount);          // co-selected
+        for (int i=0; i < n; ++i) {
+            for (int j=0; j < n; ++j) {
+                xx(i,j) = a[i] * a[j];
+            }
+        }
+        mcmat = mcmat+xx;
+
+        for (int ki=2; ki <= kint; ++ki) { // for each cluster
+            arma::mat xx(n,n,arma::fill::zeros);  // empty matix
+            IntegerVector a(cls_ml[(ki-1)]);
+            for (int i=0; i < n; ++i) {
+                for (int j=0; j < n; ++j) {
+                    xx(i,j) = a[i] * a[j];
+                }
+            }
+            mlmat = mlmat+xx;
+        }
+  return Rcpp::List::create(Rcpp::NumericMatrix(mcmat), Rcpp::NumericMatrix(mlmat));
   '
-fun <- cxxfunction(signature(a = "numeric", b = "matrix", c = "numeric", d = "numeric"), src, plugin = "RcppArmadillo")
+fun <- cxxfunction(signature(a = "list", b = "matrix", c = "list", d = "numeric", e = "numeric"), src, plugin = "RcppArmadillo")
 
 fun(c(1, 1, 1, 1, 1, 1, 2, 1), matrix(c(0), 10, 10), c(1, 2, 4, 5, 6, 7, 8, 9), 3)
 
@@ -96,17 +129,20 @@ res <- ConsensusClusterParallelPlusTest(d=mdist, cores=4, returnML=T)
 
 
 
-require(Rcpp)
-require(inline)
-require(RcppArmadillo)
-src <- '
-  Rcpp::List a(x);
-  int n = a.size();
-  for (int i=0; i<n; ++i) {
-    NumericVector b(a[i]);
-    Rcout << b << arma::endl;
-  }
-  '
-bigfun <- cxxfunction(signature(x = "list"), src, plugin = "RcppArmadillo")
+    require(Rcpp)
+    require(inline)
+    require(RcppArmadillo)
+    src <- '
+      Rcpp::List a(x);
+      int n = a.size();
+      List b(a[0]);
+      List c(a[1]);
+      NumericVector d(b[0]);
+      NumericVector e(c[0]);
+      arma::vec x(d);
+      arma::vec y(e);
+      return List::create(d, e);
+      '
+    bigfun <- cxxfunction(signature(x = "list"), src, plugin = "RcppArmadillo")
 
-bigfun(list(c(1, 1, 1, 1, 1, 1, 2, 1), c(1,2,3), c(1, 2, 4, 5, 6, 7, 8, 9)))
+    bigfun(list(list(c(1, 1, 1, 1, 1, 1, 2, 1), c(1,2,3)), list(c(1, 2, 4, 5, 6, 7, 8, 9), c(1,1,1))))
